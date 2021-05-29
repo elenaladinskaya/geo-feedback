c2bfa11072c48ef93576a2e74ebf3d7330603645
Elena Ladinskaya <ladinskaya.elena@gmail.com>
2021-05-29 13:04:11 +0300

    Сделана группировка меток

--- src/js/interactiveMap.js
+++ src/js/interactiveMap.js
@@ -28,7 +28,6 @@ export default class InteractiveMap {
 
   initMap() {
     this.clusterer = new ymaps.Clusterer({
-      groupByCoordinates: true,
       clusterDisableClickZoom: true,
       clusterOpenBalloonOnClick: false,
     });
